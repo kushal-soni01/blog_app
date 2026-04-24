fdc71c9d89cef870c6e90d05a701ff378826ba9f
kushal-soni01 <kushalsoni.ec@gmail.com>
2026-04-24 05:37:55 +0530

    Final Changes made

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7tmytks2k438ee4td#+p1bw=@)22a^9!zmuld(dyh8e7v=)+su'
 
+SECRET_KEY = config('BLOG_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
